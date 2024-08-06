c3ab9567991a0c0b2459cc3697e9fbe85bc5473b
Abhash Khadka <abhash.khadka@qniverse.co.uk>
2024-08-06 13:43:31 +0545

    CULT day 1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        byte a = 127;
+        short shot = 1;
+        int integer = 2;
+        long lng = 45678976;
+        float fl = 1.5f;
+        double d = 0.55d;
+        boolean b =true;
+        char c = 'a';
+
+        System.out.println(a);
+        System.out.println(shot);
+        System.out.println(integer);
+        System.out.println(lng);
+        System.out.println(fl);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(a+shot+integer+lng+fl+c);
+        System.out.println("bollean ="+b); //bolean lai concatnate garna mildaina raicha aru data type sanga
+
+        }
+    }
